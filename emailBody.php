26f5c8e561867f98fcc35222885444b669477b46
Tony Gaudio <wireman131@chartermi.net>
2011-03-13 18:53:16 -0400

    Sunday afternoon revisions

---
+++ emailBody.php
@@ -0,0 +1,78 @@
+<?php 
+@session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Tony's Pizza Email Confirmation</title>
+<style type="text/css">
+#confirmEmail #container {
+	width:1000px;
+	min-height:800px;
+	background-color:#C00;
+	color:black;
+	margin:0 auto;
+}
+#confirmEmail #header {
+	width:100%;
+	min-height:150px;
+	background-color:#090;
+	text-align:center;
+}
+#confirmEmail #hElement {
+	width:100%;
+	min-height:25px;
+	background-color:#000;
+}
+#confirmEmail #content {
+	position:relative;
+	width:750px;
+	padding:25px;
+	min-height:400px;
+	margin:100px;
+	background-color:#0C6;
+}
+#confirmEmail #orderSummary {
+	font-family:georgia;
+	font-weight:bold;
+	width:600px;
+	padding:50px;
+	margin:25px;
+	background-color:#FF9;
+}
+#confirmEmail #footer {
+	width:1000px;
+	height:25px;
+	margin:0 auto;
+	color:white;
+	background-color:black;
+	text-align:center;
+}
+</style>
+</head>
+
+<body>
+<div id='confirmEmail'>
+<div id='container'>
+<div id='header'>Header Image Will Go Here</div>
+<div id='hElement'></div>
+<div id='content'>
+<div id='orderSummary'>Order Summary for: <?php echo $_SESSION['firstName'] 
+    . " " . $_SESSION['lastName'] . ":<br/><br/>";
+    
+    echo $_SESSION['order'] . "<br/><br/>Order processed At :" .
+     $_SESSION['timeOfOrder'] . "<br/><br/>";
+    echo "Estimated Delivery Time : " . $_SESSION['deliveryTime'] . "<br/><br/>";
+    ?>
+    
+    
+    </div>
+</div>
+</div>
+<div id='footer'>&copy; 2011 &mdash; <strong>Tony's Pizza</strong></div>
+</div>
+
+
+</body>
+</html>
--- emailConfirm.php
+++ emailConfirm.php
@@ -3,11 +3,26 @@ session_start();
 /*
  * Pull in all session variables that were set on the order form sucess page.
  */
+
+function cleanUserData($value){  // use this to clean values prior to query
+	return mysql_real_escape_string(trim(strip_tags($value)));
+}  
+/*
+ * Sample - Temporary Data for testing:
+ */
 $_SESSION['firstName'] = "Tony";
 $_SESSION['lastName'] = "Gaudio";
 $_SESSION['address'] = "123 Main Street";
 $_SESSION['billingAddress'] = "321 South Main Street";
-$_SESSION['email'] = "j";
+$_SESSION['email'] = "wireman131@wireman131.com";
+$_SESSION['order'] = "1 Large Pizza with: Pepperoni, Green Olive, Jalepeno,
+					Bacon.  Plus one order of breadsticks.";
+$_SESSION['payMethod'] = "Cash";
+$_SESSION['emailCoupon'] = "None";
+date_default_timezone_set('America/Detroit');
+$_SESSION['timeOfOrder'] = date("l, F j, Y, g:i a");
+$_SESSION['deliveryTime'] = date("l, F j, Y, g:i a", mktime()+(30*60));
+
 function goBack(){
   //echo"Fail! <br/> Form Values Are No Good!!<br/>Try Again!";
   header('Location: pizza.php');
@@ -21,7 +36,8 @@ billingAddress
 email
 payMethod
 emailCoupon
-timeOfOrder 
+timeOfOrder
+deliveryTime
 */
 if(isset($_SESSION['firstName'])){
   $firstName = $_SESSION['firstName'];
@@ -48,18 +64,116 @@ if(isset($_SESSION['email'])){
    } else {
   goBack();
 }
-  echo "Hello " . $firstName ." " . $lastName;
-  echo "<br/>Your Address " . $address;
-  echo"<br/>Billing Address " . $billingAddress;
-  echo"<br/>Email Address " . $email;
 
+include 'emailBody.php';
 
+exit();
 /*
  * This is where the SwiftMailer will work it's magic and compose a HTML based 
  * to send as a confirmation...
  */
 
 
+include '/library/SwiftMailer/V4.0.6/lib/swift_required.php';
+
+
+try {
+  /*
+   * Create the transport object using the smtp transport method, parameters:
+   * mail host, port number, and 'ssl' sets Secure Socket Layers encryption.
+   * Below that the email account username and password is set.
+   * Store all of this information in the $transport variable
+   */
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+  ->setUsername('tonyforschool')
+  ->setPassword('advancedphp')  ;
+
+  /*
+   * Create the Mailer using your created Transport
+   * Create new mailer object - Store it in $mailer variable
+   */
+$mailer = Swift_Mailer::newInstance($transport);
+/*
+ * Create an new instance of a message - store it in the $message variable
+ */
+$message = Swift_Message::newInstance();
+/*
+ * Get existing header information frm the message object
+ */
+$headers = $message->getHeaders();
+/*
+ * add text header information to the headers object - this is viewable if you
+ * view the source of the email received.
+ */
+$headers->addTextHeader('ANM293', 'CNM-270');
+/*
+ * Set the subject of the message.
+ */
+$message->setSubject('Tony Gaudio, SWIFT Mailer 4.0.6');
+
+/*
+ * Set a From: address including a name - need to use array if more than one
+ * from address, OR if you include a name.
+ */
+$message->setFrom(array('tonyforschool@gmail.com' => 'Tony Gaudio'));
+$message->setReplyTo(array('tonyforschool@gmail.com' => 'Tony Gaudio'));
+/*
+ * Same thing goes for the To field, need to use array if more than one to
+ * address, OR if you include a name.
+ */
+$message->setTo(array(
+  'wireman131@chartermi.net' => 'Anthony Gaudio'));
+/*
+ * Bounce path for messages that can not be delivered, or a Reply To address.
+ */
+$message->setReturnPath('tonyforschool@gmail.com');
+/*
+ * Set the body of the message, followed by the format, in this case 'text/html'
+ */
+$emailConfirm = "<h1 style=\"color:red;text-align:center;\">Tony's Pizza!</h1>";
+$emailConfirm .= "<hr><h3>Order Confirmation: " . $_SESSION['order'] . " </h3>";
+$emailConfirm .= "<h4>Time of order:" . $_SESSION['timeOfOrder'] . "</h4>";
+$emailConfirm .= "<h4>Estimated Time Of Delivery : " . $_SESSION['deliveryTime']
+. "<br/>";
+$message->setBody($emailConfirm, 'text/html');
+/*
+ * Create result variable - assign it the result of the send method of the 
+ * mailer object.  result will be a number - 0 means the message failed, any
+ * other digit tells you how many messages were sucesfully delivered.
+ */
+$result = $mailer->send($message, $failures);  
+
+if (!$result)
+{
+  echo "Failures:";
+  print_r($failures);
+  /*
+   * Quoted from the support forums -->
+   * The only type of failures that will go into that array are immediate 
+   * failures such as "Relay denied", "Malformed address", 
+   * "Service unavailable" and such like errors. <--- */
+  trigger_error('Send Error Message From IF Statement : ' . $failures,E_USER_NOTICE);
+} else {
+  echo "Another amazing success story.<br/>";
+ /*
+ * Output to browser the total (%d = decimal) sent messages.
+ */
+  printf("Sent %d messages\n", $result);
+}
+
+/*
+ * Catch block for the above try.  If there is an exception anywhere above it
+ * will be assigned to $e
+ */
+  } catch(Exception $e)
+  {
+  /*
+   * I'm not sure if this is the proper way to do this, BUT it works.
+   * If there is an exception caught above, send it to the log using this.
+   */
+  trigger_error('Send Error Message: ' . $e,E_USER_NOTICE);
+  }
+  
 
 
 
