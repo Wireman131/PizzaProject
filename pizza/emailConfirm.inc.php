b197a296f7ebf4cfe018326286b14354a9db8409
Tony Gaudio <wireman131@chartermi.net>
2011-04-16 14:05:39 -0400

    directory structure change fro database

---
+++ pizza/emailConfirm.inc.php
@@ -0,0 +1,153 @@
+<?php
+@session_start();
+/*
+	* Swiftmailer email notification of order
+	*
+	* Description Long
+	*
+	* @author			Tony Gaudio, David Sullivan
+	* @category   ANM293
+	* @package    PizzaProject
+	* @version    1
+	* @link				git@github.com:Wireman131/PizzaProject
+	* @link       git@github.com:teamsullivango/PizzaProject
+	* @since      Mar 11, 2011-2011
+*/
+function cleanUserData($value){  // use this to clean values prior to query
+	return mysql_real_escape_string(trim(strip_tags($value)));
+}  
+
+date_default_timezone_set('America/Detroit');
+/*
+ * perhaps calling Date once will be enough
+ * 
+*/
+$timeOfOrder = Date("l, F j, Y, g:i a");
+$timeOfDelievery = Date("l, F j, Y, g:i a", strtotime("+30 minutes"));
+$_SESSION['timeOfOrder'] = $timeOfOrder;
+$_SESSION['deliveryTime'] = $timeOfDelievery;
+
+/*
+ * Bring in emailBody.php - Display results to browser.  If the email
+ * was successful there will be notification at the bottom of the page.
+ */
+include 'emailBody.php';
+
+//exit(); // temporary block of the emailer for testing purposes
+
+/*
+ * This is where the SwiftMailer will work it's magic and compose a HTML based 
+ * to send as a confirmation...
+ */
+
+
+include '/library/SwiftMailer/V4.0.6/lib/swift_required.php';
+
+
+try {
+  /*
+   * Create the transport object using the smtp transport method, parameters:
+   * mail host, port number, and 'ssl' sets Secure Socket Layers encryption.
+   * Below that the email account username and password is set.
+   * Store all of this information in the $transport variable
+   */
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+  ->setUsername('tonyforschool')
+  ->setPassword('advancedphp')  ;
+
+  /*
+   * Create the Mailer using your created Transport
+   * Create new mailer object - Store it in $mailer variable
+   */
+$mailer = Swift_Mailer::newInstance($transport);
+/*
+ * Create an new instance of a message - store it in the $message variable
+ */
+$message = Swift_Message::newInstance();
+/*
+ * Get existing header information frm the message object
+ */
+$headers = $message->getHeaders();
+/*
+ * add text header information to the headers object - this is viewable if you
+ * view the source of the email received.
+ */
+$headers->addTextHeader('ANM293', 'CNM-270');
+/*
+ * Set the subject of the message.
+ */
+$message->setSubject('Tony Gaudio, Pizza Order Confirmation');
+
+/*
+ * Set a From: address including a name - need to use array if more than one
+ * from address, OR if you include a name.
+ */
+$message->setFrom(array('tonyforschool@gmail.com' => 'Tony Gaudio'));
+$message->setReplyTo(array('tonyforschool@gmail.com' => 'Tony Gaudio'));
+/*
+ * Same thing goes for the To field, need to use array if more than one to
+ * address, OR if you include a name.
+ */
+$message->setTo(array(
+$_SESSION['email'] => $_SESSION['customerName']));
+  //'wireman131@chartermi.net' => 'Anthony Gaudio'));
+/*
+ * Bounce path for messages that can not be delivered, or a Reply To address.
+ */
+$message->setReturnPath('tonyforschool@gmail.com');
+/*
+ * Set the body of the message, followed by the format, in this case 'text/html'
+ */
+
+$emailConfirm = "<img src='http://localhost:8080/PizzaProject/images/header.png' />";
+$emailConfirm .= "<hr><h3>Order Confirmation: " . $_SESSION['orderSummary'] . " </h3>";
+$emailConfirm .= "<h4>Time of order:" . $_SESSION['timeOfOrder'] . "</h4>";
+$emailConfirm .= "<h4>" . $_SESSION['delivery'] . $_SESSION['deliveryTime'] . "</h4><br/>";
+$emailConfirm .= "<h4>For : " . $_SESSION['customerName'] . "</h4><br/>";
+$emailConfirm .= "<h4>Please be ready to pay $" . $_SESSION['orderTotal'] . " with "
+	. $_SESSION['payMethod'] . "</h4><br/>";
+$emailConfirm .= "<h4>Your Coupon Value : " . $_SESSION['couponValue'] . "<br/>";
+$emailConfirm .= "<h2>Thank You For Your Order!!</h2>";
+$message->setBody($emailConfirm, 'text/html');
+/*
+ * Create result variable - assign it the result of the send method of the 
+ * mailer object.  result will be a number - 0 means the message failed, any
+ * other digit tells you how many messages were sucesfully delivered.
+ */
+$result = $mailer->send($message, $failures);  
+
+if (!$result)
+{
+  echo "Failures:";
+  print_r($failures);
+  /*
+   * Quoted from the support forums -->
+   * The only type of failures that will go into that array are immediate 
+   * failures such as "Relay denied", "Malformed address", 
+   * "Service unavailable" and such like errors. <--- */
+  trigger_error('Send Error Message From IF Statement : ' . $failures,E_USER_NOTICE);
+} else {
+  echo "Another amazing success story.<br/>";
+ /*
+ * Output to browser the total (%d = decimal) sent messages.
+ */
+  printf("Sent %d messages\n", $result);
+}
+
+/*
+ * Catch block for the above try.  If there is an exception anywhere above it
+ * will be assigned to $e
+ */
+  } catch(Exception $e)
+  {
+  /*
+   * I'm not sure if this is the proper way to do this, BUT it works.
+   * If there is an exception caught above, send it to the log using this.
+   */
+  trigger_error('Send Error Message: ' . $e,E_USER_NOTICE);
+  }
+  
+
+
+
+
---
+++ pizza/library/Respect/Validation/Exceptions/ValidationException.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Respect\Validation\Exceptions;
+
+use DateTime;
+use Exception;
+use InvalidArgumentException;
+use Respect\Validation\Validatable;
+
+class ValidationException extends InvalidArgumentException
+{
+    const STANDARD = 0;
+    public static $defaultTemplates = array(
+        self::STANDARD => 'Data validation failed for %s'
+    );
+    protected $id = 'validation';
+    protected $name = '';
+    protected $template = '';
+    protected $params = array();
+
+    public static function format($template, array $vars=array())
+    {
+        return preg_replace_callback(
+            '/{{(\w+)}}/',
+            function($match) use($vars) {
+                return isset($vars[$match[1]]) ? $vars[$match[1]] : $match[0];
+            }, $template
+        );
+    }
+
+    public static function stringify($value)
+    {
+        if (is_string($value))
+            return $value;
+        elseif (is_object($value))
+            return static::stringifyObject($value);
+        else
+            return (string) $value;
+    }
+
+    public static function stringifyObject($value)
+    {
+        if (method_exists($value, '__toString'))
+            return (string) $value;
+        elseif ($value instanceof DateTime)
+            return $value->format('Y-m-d H:i:s');
+        else
+            return "Object of class " . get_class($value);
+    }
+
+    public function __toString()
+    {
+        return $this->getMainMessage();
+    }
+
+    public function chooseTemplate()
+    {
+        return key(static::$defaultTemplates);
+    }
+
+    public function configure($name, array $params = array())
+    {
+        $this->setName($name);
+        $this->setParams($params);
+        $this->message = $this->getMainMessage();
+        $this->setId($this->guessId());
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getMainMessage()
+    {
+        $vars = $this->getParams();
+        $vars['name'] = $this->getName();
+        return static::format($this->getTemplate(), $vars);
+    }
+
+    public function getParam($name)
+    {
+        return $this->hasParam($name) ? $this->params[$name] : false;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function getTemplate()
+    {
+        if (!empty($this->template))
+            return $this->template;
+        else
+            return $this->template = $this->buildTemplate();
+    }
+
+    public function hasParam($name)
+    {
+        return isset($this->params[$name]);
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName($name)
+    {
+        $this->name = static::stringify($name);
+        return $this;
+    }
+
+    public function setParam($key, $value)
+    {
+        $this->params[$key] = static::stringify($value);
+    }
+
+    public function setParams(array $params)
+    {
+        foreach ($params as $key => $value)
+            $this->setParam($key, $value);
+    }
+
+    public function setTemplate($template)
+    {
+        $this->template = $template;
+    }
+
+    protected function buildTemplate()
+    {
+        $templateKey = $this->chooseTemplate();
+        return static::$defaultTemplates[$templateKey];
+    }
+
+    protected function guessId()
+    {
+        if (!empty($this->id))
+            return;
+        $id = end(explode('\\', get_called_class()));
+        $id = lcfirst(str_replace('Exception', '', $id));
+        return $id;
+    }
+
+}
+
+/**
+ * LICENSE
+ *
+ * Copyright (c) 2009-2011, Alexandre Gomes Gaigalas.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of Alexandre Gomes Gaigalas nor the names of its
+ *       contributors may be used to endorse or promote products derived from this
+ *       software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
